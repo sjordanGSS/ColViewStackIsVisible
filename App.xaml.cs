0ea7cb8853d52736c8110c6852ade8af56074f24
Sam Jordan <sjordan@gsspos.co.uk>
2023-04-24 16:38:10 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ColViewStackIsVisible;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Input;
+
+namespace ColViewStackIsVisible;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+		collection.ItemsSource = new object[]
+		{
+			new object(),
+			new object(),
+			new object(),
+			new object()
+
+		};
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+		Resources["show-label"] = !(bool)Resources["show-label"];
+    }
+}
+
